184fcd0dcaf8c6f3f30999428a2e6ebfb0377f62
BelovVlad <vladnail30@gmail.com>
2026-09-24 14:23:45 +0700

    Refresh shell for music and PDF search changes

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Change SHELL_VERSION when changing the application shell. Material caches survive updates. */
-const SHELL_VERSION = 'v29';
+const SHELL_VERSION = 'v30';
 const BASE = new URL('./', self.location);
 const ROOT = new URL('../', BASE);
 const PREFIX = 'nsu-app-' + BASE.pathname + '-';
