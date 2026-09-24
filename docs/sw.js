fd474ac670ed07e55915358b7bca0428d0f08250
BelovVlad <vladnail30@gmail.com>
2026-09-24 15:19:51 +0700

    Refresh shell for portrait PDF quality

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Change SHELL_VERSION when changing the application shell. Material caches survive updates. */
-const SHELL_VERSION = 'v31';
+const SHELL_VERSION = 'v32';
 const BASE = new URL('./', self.location);
 const ROOT = new URL('../', BASE);
 const PREFIX = 'nsu-app-' + BASE.pathname + '-';
