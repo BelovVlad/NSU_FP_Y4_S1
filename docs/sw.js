c8ec64ae7bf1edcbf5908f946960a59a22f651f7
BelovVlad <vladnail30@gmail.com>
2026-09-24 09:48:41 +0700

    Refresh notebook chrome color cache

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Change SHELL_VERSION when changing the application shell. Material caches survive updates. */
-const SHELL_VERSION = 'v12';
+const SHELL_VERSION = 'v13';
 const BASE = new URL('./', self.location);
 const ROOT = new URL('../', BASE);
 const PREFIX = 'nsu-app-' + BASE.pathname + '-';
